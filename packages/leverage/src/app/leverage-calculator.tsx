aeec44bf878a7380b002c84bae47563af67e0021
AlexLeung <alex.l.leung@gmail.com>
2022-04-11 18:49:37 -0700

    estimates on creation:

--- packages/leverage/src/app/leverage-calculator.tsx
+++ packages/leverage/src/app/leverage-calculator.tsx
@@ -38,6 +38,45 @@ function isNumeric(value: string) {
     return (!isNaN(num) && !isNaN(value as any));
 }
 
+function getStats(trade: Trade) {
+    const entry = trade['Entry Price'];
+    const stop = trade['Stop Price'];
+    const startEquity = trade['Start Equity'];
+    const size = trade['Size'];
+    const idealExit = trade['Ideal Exit'];
+    const direction = entry < stop ? Direction.short : Direction.long;
+    function getEquity(end: number) {
+        return direction === Direction.long ?
+            size * (end - entry) + startEquity
+            :
+            size * (entry - end) + startEquity;
+    }
+    const endEquity = getEquity(idealExit);
+    const equityAtStop = getEquity(stop);
+    const startLev = (size * entry) / startEquity;
+    const endLev = (size * idealExit) / endEquity;
+    const stopLev = (size * stop) / equityAtStop;
+    const liqPrice = direction === Direction.long ?
+        entry - (startEquity / size)
+        :
+        entry + (startEquity / size)
+    ;
+    const profitLoss = endEquity - startEquity;
+    const change = profitLoss / startEquity;
+    const stopLoss = equityAtStop - startEquity;
+    const maxLoss = (startEquity - equityAtStop) / startEquity * 100;
+    
+    // Mango start
+    //   TODO: maintenance leverage is 40x for SOL, BTC, and ETH perps but smaller for other perps and operates by a different mechanism for spot (seemingly).
+    //         fix this by adding a trading platform input and an asset market.
+    const liquidationLeverage = 40;
+    const equityDirection = direction === Direction.long ? -1 : 1;
+    const mangoLiqPrice = ((equityDirection * liquidationLeverage * entry * size) + (liquidationLeverage * startEquity)) / (size + (liquidationLeverage * size * equityDirection));
+    // Mango end
+    
+    return {entry, stop, startEquity, size, idealExit, direction, endEquity, equityAtStop, startLev, endLev, stopLev, liqPrice, change, stopLoss, maxLoss, mangoLiqPrice, profitLoss};
+}
+
 // Features:
 // 1. set entry
 // 2. set exit
@@ -63,10 +102,13 @@ export const LeverageCalculator: React.FC = () => {
         setTrades(newTrades);
         window.localStorage.setItem(localstoragekey, JSON.stringify(newTrades));
     }
+    function inputsToTrade(): Trade {
+        return Object.fromEntries(Object.entries(inputs).map(([field, value]) => [field, parseFloat(value)])) as {[f in UpdateableFields]: number};
+    }
     function addTrade() {
         if (!active()) return;
         const newTrades = [...trades];
-        newTrades.push(Object.fromEntries(Object.entries(inputs).map(([field, value]) => [field, parseFloat(value)])) as {[f in UpdateableFields]: number});
+        newTrades.push(inputsToTrade());
         saveTrades(newTrades);
         clear();
     }
@@ -136,47 +178,13 @@ export const LeverageCalculator: React.FC = () => {
                         <th>Mango Liq Price</th>
                     </tr>
                     {trades.map((trade, index) => {
-                        const entry = trade['Entry Price'];
-                        const stop = trade['Stop Price'];
-                        const startEquity = trade['Start Equity'];
-                        const size = trade['Size'];
-                        const idealExit = trade['Ideal Exit'];
-                        const direction = entry < stop ? Direction.short : Direction.long;
-                        function getEquity(end: number) {
-                            return direction === Direction.long ?
-                                size * (end - entry) + startEquity
-                                :
-                                size * (entry - end) + startEquity;
-                        }
-                        const endEquity = getEquity(idealExit);
-                        const equityAtStop = getEquity(stop);
-                        const startLev = (size * entry) / startEquity;
-                        const endLev = (size * idealExit) / endEquity;
-                        const stopLev = (size * stop) / equityAtStop;
-                        const liqPrice = direction === Direction.long ?
-                            entry - (startEquity / size)
-                            :
-                            entry + (startEquity / size)
-                        ;
-                        const profitLoss = endEquity - startEquity;
-                        const change = profitLoss / startEquity;
-                        const stopLoss = equityAtStop - startEquity;
-                        const maxLoss = (startEquity - equityAtStop) / startEquity * 100;
                         function updateTrade(newTrade: Trade) {
                             const newTrades = [...trades];
                             newTrades[index] = newTrade;
                             saveTrades(newTrades);
                         }
                         const beingEdited = index === editing;
-
-                        // Mango start
-                        //   TODO: maintenance leverage is 40x for SOL, BTC, and ETH perps but smaller for other perps and operates by a different mechanism for spot (seemingly).
-                        //         fix this by adding a trading platform input and an asset market.
-                        const liquidationLeverage = 40;
-                        const equityDirection = direction === Direction.long ? -1 : 1;
-                        const mangoLiqPrice = ((equityDirection * liquidationLeverage * entry * size) + (liquidationLeverage * startEquity)) / (size + (liquidationLeverage * size * equityDirection));
-                        // Mango end
-
+                        const stats = getStats(trade);
                         return (
                             <tr key={index} style={beingEdited ? {backgroundColor: 'lightblue'} : {}}>
                                 <td>
@@ -189,16 +197,8 @@ export const LeverageCalculator: React.FC = () => {
                                 {Object.values(UpdateableFields).map(field =>
                                     <Updateable trade={trade} onChange={updateTrade} k={field} key={field} editing={beingEdited} />
                                 )}
-                                <th>{direction}</th>
-                                <td>{dec2(startLev)}</td>
-                                <td>{dec2(endLev)}</td>
-                                <td>{dec2(stopLev)}</td>
-                                <td>{dec2(endEquity)}</td>
-                                <td>{dec2(equityAtStop)}</td>
-                                <td>{dec2(profitLoss)}<br />({dec2(change * 100)}%)</td>
-                                <td>{dec2(stopLoss)}<br />({-dec2(maxLoss)}%)</td>
-                                <td>{dec2(liqPrice)}</td>
-                                <td>{dec2(mangoLiqPrice)}</td>
+                                <th>{stats.direction}</th>
+                                <StatsDisplay stats={stats} />
                                 <td><button onClick={() => { deleteTrade(index) }}>X</button></td>
                                 <td><button onClick={() => { duplicateTrade(index) }}>❐</button></td>
                                 {beingEdited && <>                                   
@@ -215,12 +215,25 @@ export const LeverageCalculator: React.FC = () => {
                         </td>
                         {Object.values(UpdateableFields).map(field => <FormField key={field} onChange={val => { setInputs({...inputs, [field]: val }) }} value={inputs[field]} />)}
                         <th>{ (isNumeric(inputs['Entry Price']) && isNumeric(inputs['Stop Price'])) ? parseFloat(inputs['Entry Price']) > parseFloat(inputs['Stop Price']) ? Direction.long : Direction.short : "--" }</th>
+                        {active() && <StatsDisplay stats={getStats(inputsToTrade())} />}
                     </tr>
                 </tbody>
             </table>
         </div>
     )
 }
+
+const StatsDisplay: React.FC<{stats: ReturnType<typeof getStats>}> = ({stats}) => <>
+    <td>{dec2(stats.startLev)}</td>
+    <td>{dec2(stats.endLev)}</td>
+    <td>{dec2(stats.stopLev)}</td>
+    <td>{dec2(stats.endEquity)}</td>
+    <td>{dec2(stats.equityAtStop)}</td>
+    <td>{dec2(stats.profitLoss)}<br />({dec2(stats.change * 100)}%)</td>
+    <td>{dec2(stats.stopLoss)}<br />({-dec2(stats.maxLoss)}%)</td>
+    <td>{dec2(stats.liqPrice)}</td>
+    <td>{dec2(stats.mangoLiqPrice)}</td>
+</>
 
 const FormField: React.FC<{value: string; onChange: (s: string) => void}> = ({onChange, value}) => {
     return (
